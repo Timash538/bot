c3c8f8ac51d532ee248087e55e8c99003f14af14
Timash <arthurmehovoy@gmail.com>
2023-09-05 15:43:55 +0300

    bot2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import telebot
 bot = telebot.TeleBot('6529797726:AAHpCo7HqrCcQX1VfTMyuoHysCM2HTxpHC4')
 
-@bot.message_handler(content_type=['text'])
+@bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    bot.send_message(message.chat.id, "Иди нахер")
+    bot.send_message(message.chat.id, 'Иди нахер')
 
 bot.infinity_polling()
