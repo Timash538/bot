177101331eb3099378736085fa87d94e890bfa2f
Timash <arthurmehovoy@gmail.com>
2023-09-06 09:58:36 +0300

    bot3

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import telebot
+import random
 bot = telebot.TeleBot('6529797726:AAHpCo7HqrCcQX1VfTMyuoHysCM2HTxpHC4')
 
-@bot.message_handler(content_types=['text'])
+users = dict()
+cables = ['5x6','5x10','3x1,5','3x2,5','3x4','3x10','5x16']
+
+@bot.message_handler(command=['cable'])
 def get_text_messages(message):
-    bot.send_message(message.chat.id, 'Иди нахер')
+    if message.from_user in users:
+        cable = random.choice(cables)
+        users[message.from_user] = cable
+    bot.send_message(message.chat.id, 'Твой кабель: ', users.get(message.from_user))
 
 bot.infinity_polling()
