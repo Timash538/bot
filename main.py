1f3e5c0f52a82a3dfa467fdf3674e68b1f3c111b
Timash <arthurmehovoy@gmail.com>
2023-09-06 10:04:40 +0300

    bot4

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ cables = ['5x6','5x10','3x1,5','3x2,5','3x4','3x10','5x16']
 
 @bot.message_handler(command=['cable'])
 def get_text_messages(message):
-    if message.from_user in users:
+    if message.from_user not in users:
+        print('if сработало')
         cable = random.choice(cables)
         users[message.from_user] = cable
     bot.send_message(message.chat.id, 'Твой кабель: ', users.get(message.from_user))
