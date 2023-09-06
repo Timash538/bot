0ef8162bef91151975df863940af8bd719240349
Timash <arthurmehovoy@gmail.com>
2023-09-06 10:06:11 +0300

    bot5

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot('6529797726:AAHpCo7HqrCcQX1VfTMyuoHysCM2HTxpHC4')
 users = dict()
 cables = ['5x6','5x10','3x1,5','3x2,5','3x4','3x10','5x16']
 
-@bot.message_handler(command=['cable'])
+@bot.message_handler(context_types=['text'])
 def get_text_messages(message):
     if message.from_user not in users:
         print('if сработало')
