295765832320f0acec0ae4c77e40f8fe3dba2791
Timash <arthurmehovoy@gmail.com>
2023-09-06 10:44:09 +0300

    'bot'

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ users = dict()
 cables = ['5x6','5x10','3x1,5','3x2,5','3x4','3x10','5x16']
 print('zdes ok')
 
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(commands=['cable'])
 def get_text_messages(message):
     print('event srabotalo')
-    cable = random.choice(cables)
-    users[message.from_user.id] = cable
-    bot.send_message(message.chat.id, 'Твой кабель: ', users.get(message.from_user.id))
+    if message.from_user.id not in users:
+        print('if srabotalo')
+        cable = random.choice(cables)
+        print(cable)
+        users[message.from_user.id] = cable
+    text = 'Твой кабель: ' + users.get(message.from_user.id)
+    bot.send_message(message.chat.id, text)
 
 bot.infinity_polling()
---
+++ push.bat
@@ -0,0 +1,3 @@
+git add -A
+git commit -m'bot'
+git push
