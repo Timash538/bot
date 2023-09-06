e09e6fe2f3d1cf9ebedadd8b90dd789d3d9992c1
Timash <arthurmehovoy@gmail.com>
2023-09-06 10:07:43 +0300

    bot6

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ cables = ['5x6','5x10','3x1,5','3x2,5','3x4','3x10','5x16']
 
 @bot.message_handler(context_types=['text'])
 def get_text_messages(message):
-    if message.from_user not in users:
-        print('if сработало')
-        cable = random.choice(cables)
-        users[message.from_user] = cable
+    print('if сработало')
+    cable = random.choice(cables)
+    users[message.from_user] = cable
     bot.send_message(message.chat.id, 'Твой кабель: ', users.get(message.from_user))
 
 bot.infinity_polling()
